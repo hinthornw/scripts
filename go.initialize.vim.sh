4eeeb31a592ca406b1811c8a6d8806898c68b011
William Fu-Hinthorn <whinthorn@gmail.com>
2020-08-03 11:59:45 -0700

    Create go.initialize.vim.sh
    Added vim-sensible to vim environment setup

---
+++ go.initialize.vim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "execute pathogen#infect()" >> ~/.vimrc
+echo "syntax on" >> ~/.vimrc
+echo "filetype plugin indent on" >> ~/.vimrc
+
+cd ~/.vim/bundle && \
+git clone https://github.com/tpope/vim-sensible.git
